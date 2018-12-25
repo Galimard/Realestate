b28c97eae86c30f047e887f7fbbfd3be6beb91d9
Maria <galimard.maria@gmail.com>
2018-12-25 14:16:41 +0800

    #add добавлены стили для слайдера, без инициализации

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    //
+    // if($('.carousel')) {
+    //     $('.carousel').slick({
+    //         arrows: false,
+    //         appendDots: $('.carousel__dots'),
+    //         infinite: true,
+    //         dots: true,
+    //         // autoplay: true,
+    //         autoplaySpeed: 3000
+    //     });
+    // }
+
+});
