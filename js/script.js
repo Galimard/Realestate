d5c61f4555db3c3640ac1ff90a1c4d11d70ed0d5
Maria <galimard@yandex.ru>
2019-01-01 13:17:04 +0800

    #add первая проба пофвления формы регистрации

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    //вехрний слайдер
     if($('.carousel')) {
         $('.carousel').slick({
             arrows: false,
@@ -11,8 +12,10 @@ $(document).ready(function() {
         });
     }
 
+    //кастомищация селектов
     $('.search__select').styler();
 
+    //слайдер в футере
     if($('.costamer__slider')) {
         $('.costamer__slider').slick({
             arrows: true,
@@ -21,4 +24,24 @@ $(document).ready(function() {
             dots: false
         });
     }
+
+    //форма регистрации в блоке registration
+    //открыть
+    $('.registration__signup').click( function(event){
+        event.preventDefault();
+        $('.popup__overlay').fadeIn(400, function(){
+                $('.popup__modal').css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
+            });
+    });
+
+    //закрыть
+    $('.popup__overlay, .popup__close').click( function(){
+        $('.popup__modal')
+            .animate({opacity: 0, top: '45%'}, 200, function(){
+                    $(this).css('display', 'none');
+                    $('#overlay').fadeOut(400);
+                }
+            );
+    });
+
 });
