784c93969a8db94cbf373ebd5b7f1c073630c702
Maria <galimard@yandex.ru>
2019-01-02 15:01:17 +0800

    #fix исправлены скрипты модального окна и активного пункта меню

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
 $(document).ready(function() {
 
-    //вехрний слайдер
-    if($('.carousel')) {
-        $('.carousel').slick({
-            arrows: false,
-            appendDots: $('.carousel__dots'),
-            infinite: true,
-            dots: true,
-            // autoplay: true,
-            autoplaySpeed: 3000
-        });
-    }
+    //активная вкладка меню
+    $('.header-menu-list__item').click(function() {
+        var item =  $('.header-menu-list__item');
+
+        $(item).siblings().removeClass('header-menu-list__item--active');
+        $(this).addClass('header-menu-list__item--active');
+    });
+
+    //бургер меню
+    $('.header-menu__hamburger').click(function(event) {
+        event.preventDefault();
+
+        var menu = $('.header-menu__navigation2'),
+               display = menu.css('display');
+
+        if(display === 'none') {
+            menu.css('display', 'inline-block')
+        } else {
+            menu.css('display', 'none');
+        }
+
+        //крестик
+        $('.line__main').toggleClass('burger-line2');
+        $('.line__middle').toggleClass('burger-line3');
+
+    });
+
+    //верхний слайдер
+    // if($('.carousel')) {
+    //     $('.carousel').slick({
+    //         arrows: false,
+    //         appendDots: $('.carousel__dots'),
+    //         infinite: true,
+    //         dots: true,
+    //         // autoplay: true,
+    //         autoplaySpeed: 3000
+    //     });
+    // }
 
     //кастомищация селектов
     $('.search__select').styler();
@@ -30,7 +57,7 @@ $(document).ready(function() {
     $('.registration__signup').click( function(event){
         event.preventDefault();
         $('.popup__overlay').fadeIn(400, function(){
-                $('.popup__modal').css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
+                $('.popup__modal').css('display', 'block').animate({opacity: 1, top: '45%'}, 200);
             });
     });
 
@@ -39,7 +66,7 @@ $(document).ready(function() {
         $('.popup__modal')
             .animate({opacity: 0, top: '45%'}, 200, function(){
                     $(this).css('display', 'none');
-                    $('#overlay').fadeOut(400);
+                    $('.popup__overlay').fadeOut(400);
                 }
             );
     });
